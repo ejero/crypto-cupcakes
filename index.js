12ca9075366cd374fe23db5185c6114fd4ba6ba3
Rosita E <RositaEmakpo@Gmail.com>
2023-03-19 14:49:32 -0700

    Add env file and Auth0

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+  const {
+    AUTH0_SECRET, AUTH0_AUDIENCE='http://localhost:3000', AUTH0_CLIENT_ID, AUTH0_BASE_URL} = process.env;
+  
+
+  const { auth } = require('express-openid-connect');
+
+  const config = {
+    authRequired: true,
+    auth0Logout: true,
+    secret: AUTH0_SECRET,
+    baseURL: AUTH0_AUDIENCE,
+    clientID: AUTH0_CLIENT_ID,
+    issuerBaseURL: AUTH0_BASE_URL,
+  };
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
@@ -30,6 +55,7 @@ app.get('/cupcakes', async (req, res, next) => {
     next(error);
   }
 });
+
 
 // error handling middleware
 app.use((error, req, res, next) => {
